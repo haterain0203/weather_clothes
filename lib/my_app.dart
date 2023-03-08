986e318ff41853f1ab5bc2d01b64bcbca25e8c21
haterain0203 <haterain0203@gmail.com>
2023-03-08 12:18:56 +0900

    Sizer追加

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:sizer/sizer.dart';
 
 import 'home_page.dart';
 
@@ -8,12 +10,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather_clothes',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        // intl初期化のためのメソッドを追加
+        initializeDateFormatting('ja');
+        return MaterialApp(
+          title: 'Weather_clothes',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
