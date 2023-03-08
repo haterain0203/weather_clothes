3fbc0ee311935c4b28e9c4019a944f5e4497369a
haterain0203 <haterain0203@gmail.com>
2023-03-08 12:09:15 +0900

    急プロジェクトから引用

---
+++ lib/component/rounded_corner_container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class RoundedCornerContainer extends StatelessWidget {
+  const RoundedCornerContainer({
+    this.onTap,
+    this.height,
+    this.width,
+    this.color,
+    this.padding,
+    this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final VoidCallback? onTap;
+  final double? height;
+  final double? width;
+  final Color? color;
+  final EdgeInsets? padding;
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        //height指定がない場合は160.0
+        height: height,
+        //width指定がない場合は画面横幅いっぱい
+        width: width ?? double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          //color指定がない場合はホワイト
+          color: color ?? Colors.white,
+        ),
+        padding: padding,
+        child: child,
+      ),
+    );
+  }
+}
