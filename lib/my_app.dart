e2dfc66171a271176504e964d453b21dc902f9ad
haterain0203 <haterain0203@gmail.com>
2023-03-08 11:51:42 +0900

    flutter_version変更

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather_clothes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
