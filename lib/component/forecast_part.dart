e312ba8aa039fe20c10a0790af4223928a0c13eb
haterain0203 <haterain0203@gmail.com>
2023-03-11 15:27:52 +0900

    HomePageから値を受けることでStatelessWidgetに変更

--- lib/component/forecast_part.dart
+++ lib/component/forecast_part.dart
@@ -1,45 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:weather_clothes/component/yesterday_or_tomorrow_container.dart';
-import 'package:weather_clothes/feature/weather/weather_controller.dart';
 
-class ForecastPart extends HookConsumerWidget {
+import '../feature/weather/weather.dart';
+
+class ForecastPart extends StatelessWidget {
   const ForecastPart({
+    required this.weather,
     super.key,
   });
+  final Weather weather;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final weather = ref.watch(weatherProvider);
-    return weather.when(
-      loading: () => const Center(
-        child: CircularProgressIndicator(),
-      ),
-      error: (error, stack) {
-        print('error = $error');
-        return Center(
-          child: Text('天気情報取得時にエラーが発生しました: $error'),
-        );
-      },
-      data: (data) {
-        return Row(
-          children: [
-            YesterdayOrTomorrowContainer(
-              // 昨日の日付
-              date: DateTime.now().subtract(const Duration(days: 1)),
-              temperature: data.temperature.yesterdayTemperature,
-            ),
-            const SizedBox(
-              width: 16,
-            ),
-            YesterdayOrTomorrowContainer(
-              // 明日の日付
-              date: DateTime.now().subtract(const Duration(days: -1)),
-              temperature: data.temperature.tomorrowTemperature,
-            ),
-          ],
-        );
-      },
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        YesterdayOrTomorrowContainer(
+          // 昨日の日付
+          date: DateTime.now().subtract(const Duration(days: 1)),
+          temperature: weather.temperature.yesterdayTemperature,
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        YesterdayOrTomorrowContainer(
+          // 明日の日付
+          date: DateTime.now().subtract(const Duration(days: -1)),
+          temperature: weather.temperature.tomorrowTemperature,
+        ),
+      ],
     );
   }
 }
